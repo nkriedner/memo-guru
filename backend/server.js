3879bccc6aa87d290a180320155a91fb510d9991
Nils Kriedner <nkriedner@googlemail.com>
2024-03-15 20:52:06 +0100

    Add middleware to log method and path for every request

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const express = require("express");
 // Create the app / server
 const app = express();
 
+// Middleware
+app.use((req, res, next) => {
+    // log the req method and path for every request
+    console.log(`${req.method} request to "${req.path}"`);
+    next();
+});
+
 // Create the routes
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to MEMO-GURU!" });
