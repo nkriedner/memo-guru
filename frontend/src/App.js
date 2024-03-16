d774561e6ccc3ee27a35227ce0f99ec924267478
Nils Kriedner <nkriedner@googlemail.com>
2024-03-16 18:34:29 +0100

    Use React Browserrouter to create Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // for route handling
+
+// Import pages & components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
     return (
         <div className="App">
-            <h1>MemoGuru</h1>
+            <BrowserRouter>
+                <Navbar />
+                <div className="pages">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
