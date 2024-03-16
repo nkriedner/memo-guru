f05ee98a943e9a514871bc5a94a3b2f4107da549
Nils Kriedner <nkriedner@googlemail.com>
2024-03-16 18:58:50 +0100

    Fetch cards data from App component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+    // create state for card data
+    const [cards, setCards] = useState(null);
+
+    useEffect(() => {
+        const fetchCards = async () => {
+            // fetch card data from the backend
+            const response = await fetch("/api/cards");
+            // parse the json data into an array of objects
+            const json = await response.json();
+
+            // check if the data is ok
+            if (response.ok) {
+                setCards(json);
+            }
+        };
+
+        fetchCards();
+    }, []);
+
     return (
         <div className="home">
             <h2>Home</h2>
+            <div className="cards">
+                {/* only when there is card data: */}
+                {cards && cards.map((card) => <p key={card._id}>{card.content_1}</p>)}
+            </div>
         </div>
     );
 };
